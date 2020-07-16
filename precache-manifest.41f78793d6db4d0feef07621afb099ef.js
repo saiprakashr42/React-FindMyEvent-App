69470e10b33c57ccf40df4fc8b57c315de717743
SAI PRAKASH <45858006+saiprakashr42@users.noreply.github.com>
2020-07-16 19:00:38 +0530

    Updates

---
+++ precache-manifest.41f78793d6db4d0feef07621afb099ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4214caeac5118a689da8a3675b42664",
+    "url": "/React-FindMyEvent-App/index.html"
+  },
+  {
+    "revision": "89d71a2d159466c37fe2",
+    "url": "/React-FindMyEvent-App/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a4986cb09b73ad8dfa07",
+    "url": "/React-FindMyEvent-App/static/css/main.d522e94b.chunk.css"
+  },
+  {
+    "revision": "89d71a2d159466c37fe2",
+    "url": "/React-FindMyEvent-App/static/js/2.4f6a7d0f.chunk.js"
+  },
+  {
+    "revision": "544111c69119227bbf075a65771ccdb1",
+    "url": "/React-FindMyEvent-App/static/js/2.4f6a7d0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4986cb09b73ad8dfa07",
+    "url": "/React-FindMyEvent-App/static/js/main.c762ab94.chunk.js"
+  },
+  {
+    "revision": "de226cb0cbac5f716128",
+    "url": "/React-FindMyEvent-App/static/js/runtime-main.fe96f370.js"
+  }
+]);
